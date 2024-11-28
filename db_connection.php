2a57c7be4f49a5403649e09dab56bab4f8719eea
Abdurrahman98-web <a.ferid1918@gmail.com>
2024-11-28 12:37:12 +0400

    add JAWSDB_URL to the zoo-app project

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "login_db";
+// Heroku'nun DATABASE_URL ortam değişkenini al
+$databaseUrl = parse_url(getenv('JAWSDB_URL'));
 
+if ($databaseUrl) {
+    // DATABASE_URL varsa Heroku'da çalışıyoruz
+    $url = parse_url($databaseUrl);
+
+    $servername = $url['host'];
+    $username = $url['user'];
+    $password = $url['pass'];
+    $dbname = ltrim($url['path'], '/');
+} else {
+    // Yerel ortam için bağlantı bilgileri
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "login_db";
+}
+
+// MySQL bağlantısı oluşturuluyor
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Bağlantı kontrolü
 if ($conn->connect_error) {
     die("Veritabanı bağlantısı başarısız: " . $conn->connect_error);
 }
 ?>
+
--- submit_login.php
+++ submit_login.php
@@ -3,7 +3,6 @@ session_start();
 require 'db_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
     $role = $_POST['role'];
     $username = $_POST['username'];
     $password = $_POST['password'];
