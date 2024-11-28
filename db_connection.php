6f7b527481718c069dfca9836d0340d50d0d0733
Abdurrahman98-web <a.ferid1918@gmail.com>
2024-11-28 21:15:55 +0400

    update db-connections

--- db_connection.php
+++ db_connection.php
@@ -1,20 +1,20 @@
-<?php
-// Ortam değişkenini kullanarak veritabanına bağlanma
-$dsn = getenv('JAWSDB_URL'); // JAWSDB_URL'yi al
-$parsedUrl = parse_url($dsn);
+<?php // Ortam değişkenini kullanarak veritabanına bağlanma
+$JAWSDB_URL = 'mysql://x2ahu8zx4dd4j2w5:ph27tkiggs3xfvha@d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pliiurw8rqid4ocx';
+$parsedUrl = parse_url($JAWSDB_URL);
+
+if (!$parsedUrl || !isset($parsedUrl['host'], $parsedUrl['user'], $parsedUrl['pass'], $parsedUrl['path'], $parsedUrl['port'])) {
+    die("Geçersiz veritabanı bağlantı bilgileri.");
+}
 
 $host = $parsedUrl['host'];
 $username = $parsedUrl['user'];
 $password = $parsedUrl['pass'];
 $database = ltrim($parsedUrl['path'], '/');
 $port = $parsedUrl['port'];
-
-// Veritabanına bağlanma
 $conn = new mysqli($host, $username, $password, $database, $port);
 if ($conn->connect_error) {
     die("Bağlantı hatası: " . $conn->connect_error);
 }
-echo "Veritabanı bağlantısı başarılı!";
 ?>
 
 
