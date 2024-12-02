e300238ef535d26bc4ed9bed78114b00483bf976
Abdurrahman98-web <a.ferid1918@gmail.com>
2024-12-02 17:25:04 +0400

    composer.json güncellemeleri ve composer.lock güncellemesi

--- db_connection.php
+++ db_connection.php
@@ -18,7 +18,7 @@ if ($dbUrl) {
 }
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo =$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Veritabanı bağlantısı başarısız: " . $e->getMessage());
